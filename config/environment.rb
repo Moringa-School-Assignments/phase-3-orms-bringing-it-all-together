6812a9941320c6a7844f85b5fe8365e9a1c31105
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-11-04 16:19:38 +0300

    complete assignment

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,20 @@ Bundler.require
 
 require_relative '../lib/dog'
 
-DB = { conn: SQLite3::Database.new("db/dogs.db") }
+DB = { conn: SQLite3::Database.new("../db/dogs.db") }
+# props = {
+#     name: "James",
+#     breed: "Oscar",
+#     id: nil
+# }
+
+# obj = Dog.new(name: "James", breed: "Oscar", id: nil)
+obj = Dog.new(name: "Carter", breed: "Punjabi", id: nil)
+
+p obj.breed
+# Dog.create_table
+p Dog.find_by_name "James"
+p Dog.find 2
+
+# obj.save "James"
+
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,74 @@
 class Dog
+    attr_accessor :name, :breed, :id
+
+    def initialize(name:, breed:, id:)
+        @name = name 
+        @breed = breed 
+        @id = id
+    end
+
+    def self.create_table
+        drop = <<-SQL
+        DROP TABLE IF EXISTS dogs;
+        SQL
+
+        DB[:conn].execute(drop)
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS dogs(
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            breed TEXT
+        )
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def self.drop_table
+        sql = <<-SQL
+        DROP TABLE IF EXISTS dogs;
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        sql = <<-SQL
+            INSERT INTO dogs (name, breed)
+            VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.breed)
+    end
+
+    def self.create
+        save 
+    end
+
+    def self.new_from_db
+        self.new(name:, breed:, id:)
+    end
+
+    def self.all 
+        sql = <<-SQL
+            SELECT * FROM dogs;
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+    def self.find_by_name find_name
+        sql = <<-SQL
+            select * FROM dogs WHERE name="#{find_name}"
+        SQL
+
+        DB[:conn].execute(sql)
+    end
+
+    def self.find id
+        sql = <<-SQL
+            SELECT * FROM dogs WHERE id="#{id}"
+        SQL
+        DB[:conn].execute(sql)
+    end
 
 end
+
